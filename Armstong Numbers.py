968461ebc18b5addfe388b47e26d17656ff541f1
AnonymousNA <39471698+AnonymousNA@users.noreply.github.com>
2023-02-13 22:07:32 -0800

    Update Armstong Numbers.py
    Commented to add more transparency. Also I renamed some variables to reflect their purpose better.

--- Armstong Numbers.py
+++ Armstong Numbers.py
@@ -1,28 +1,30 @@
 # An armstrong number is a number whose digits squared by the number of digits is equal to the number
 
-number_int = int(input("Upper bound: "))
-num = number_int
-armstrong_numbers = []
+def main():
+    number = input("Upper bound: ")
+    number_int = int(number)    # number_int is used to allow a mutable number value to increment through
+    armstrong_numbers = []
 
-while number_int > 0:
-    parse = number_int  # Parse is a temp variable that is only used to increment num_digits
-    num_digits = 0  # The power of 10 of the number
-    number_str = str(number_int)
+    while number_int > 0:
+        parse = number_int  # Parse is a temp variable that is only used to increment power
+        power = 0  # The power of 10 of the number
+        number_str = str(number_int)
+        temp = []
 
-    while parse >= 1:  # This will set the num_digits
-        parse = parse/10
-        num_digits += 1
+        while parse >= 1:  # Sets the number of digits each iteration to the power var
+            parse = parse/10
+            power += 1
 
-    temp = []
-    for iterate in number_str:
-        temp.append(int(iterate)**num_digits)
+        for i in number_str:  # makes a list of all the digits to the power of the amount of digits
+            temp.append(int(i)**power)
 
-    if sum(temp) == number_int:
-        armstrong_numbers.append(sum(temp))
+        if sum(temp) == number_int:    # Compares the sum of all the digits to the power of number of digits to the original number
+            armstrong_numbers.append(sum(temp))
+            
+        number_int -= 1    # Needed to increment down for each iteration
 
-    # print(number_int, num_digits, temp, sep=' | ')
-    number_int -= 1
+    print("All armstrong numbers from 0 to", number, "are:")
+    for i in armstrong_numbers:    # Prints the list in a nice format.
+        print(i, end=' ')
 
-print("All armstrong numbers from 0 to", num, "are:")
-for i in armstrong_numbers:
-    print(i, end=' ')
+main()
